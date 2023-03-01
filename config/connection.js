1f0eca40cc31943a863a8ddd3e7ad15dd4ae2781
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-03-01 17:01:27 +0000

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,15 @@
+// Import necessary modules
 require('dotenv').config();
-
 const Sequelize = require('sequelize');
 
+// Set up database connection
 const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+  ? new Sequelize(process.env.JAWSDB_URL) // If using JawsDB for hosting, connect to it
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
+      host: 'localhost', // If running locally, set host to 'localhost'
+      dialect: 'mysql', // Set dialect to MySQL
+      port: 3306 // Set port number to 3306 (default MySQL port)
     });
     
+// Export the configured database connection
 module.exports = sequelize;
