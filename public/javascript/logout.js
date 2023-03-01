e70ac456f3466c9825c81f9586c7c0b367698dfd
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-03-01 19:35:34 +0000

    Update logout.js

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,14 +1,19 @@
-async function logout() {
+const logoutButton = document.querySelector('#logout');
+
+logoutButton.addEventListener('click', async () => {
+  try {
     const response = await fetch('/api/users/logout', {
-      method: 'post',
+      method: 'POST',
       headers: { 'Content-Type': 'application/json' }
     });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
+
+    if (!response.ok) {
+      throw new Error('Failed to logout');
     }
+
+    document.location.replace('/');
+  } catch (error) {
+    console.error(error);
+    alert('Failed to logout');
   }
-  
-  document.querySelector('#logout').addEventListener('click', logout);
+});
