e4703d5bb63497e9b1167d986e6419567b52cedd
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-03-01 19:03:29 +0000

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,38 +1,45 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
 // create our Post model
 class Post extends Model {
+
   static upvote(body, models) {
+    // create a new vote
     return models.Vote.create({
       user_id: body.user_id,
       post_id: body.post_id
-    }).then(() => {
-      return Post.findOne({
-        where: {
-          id: body.post_id
-        },
-        attributes: [
-          'id',
-          'post_url',
-          'title',
-          'created_at',
-          [
-            sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-            'vote_count'
-          ]
-        ],
-        include: [
-          {
-            model: models.Comment,
-            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-            include: {
-              model: models.User,
-              attributes: ['username']
+    })
+      .then(() => {
+        // find the post
+        return Post.findOne({
+          where: {
+            id: body.post_id
+          },
+          // query configuration
+          attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [
+              sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+              'vote_count'
+            ]
+          ],
+          include: [
+            // include the Comment model here:
+            {
+              model: models.Comment,
+              attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+              include: {
+                model: models.User,
+                attributes: ['username']
+              }
             }
-          }
-        ]
+          ]
+        });
       });
-    });
   }
 }
 
