ca8373ef15ae2a0fd20a6ed36848161448b79a1d
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-03-01 17:09:01 +0000

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api/');
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
+const homeRoutes = require('./HomeRoutes.js');
 
-router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
+router.use('/', HomeRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
