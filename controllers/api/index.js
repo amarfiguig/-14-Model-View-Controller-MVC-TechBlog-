b916a0bf47035d09c0cf68f7080f809be3f191b2
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-03-01 17:29:46 +0000

    Update index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,13 @@
+// Importing the required packages and modules
 const router = require('express').Router();
-
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
 
+// Adding middleware routes for users, posts, and comments
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 
+// Exporting the router
 module.exports = router;
